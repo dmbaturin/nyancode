3a854e362ff66d5979e1c72f1b20ea486598d59f
Daniil Baturin <daniil@baturin.org>
2015-01-11 03:25:00 +0600

    Translate tokens while lexing the source, remove no-op's.

--- nyanc.ml
+++ nyanc.ml
@@ -24,27 +24,13 @@ value = 0;\n\
 output = $smake(1);
 "
 
-(* Read source and break it into tokens *)
-let read_source filename = 
-    let tokens = ref [] in
-    let chan = open_in filename in
-    try
-        while true do
-            let line = input_line chan in
-            let line_tokens = Str.split (Str.regexp " ") line in 
-            tokens := !tokens @ line_tokens
-        done; []
-    with End_of_file ->
-      close_in chan;
-    !tokens
-
 (* Nesting level checking stuff. We don't track positions here,
    just check if the number of left brackets equals the number
    or right ones. *)
 let update_nesting_level token level =
     match token with
-    | "nyaaaan" -> level + 1
-    | "Nyaaaan" -> level - 1
+    | LoopStart -> level + 1
+    | LoopEnd -> level - 1
     | _         -> level
 
 let rec check_nesting_level ?(level=0) source =
@@ -76,8 +62,15 @@ let op_of_token token =
     | "Nyaaaan" -> LoopEnd
     | _         -> Nop
 
-let ops_of_tokens tokens =
-    List.map op_of_token tokens
+let ops_of_tokens words =
+    let rec loop words acc =
+        match words with
+        | [] -> acc
+        | x :: xs ->
+            let op = op_of_token x in
+            if op = Nop then loop xs acc
+            else loop xs (op :: acc)
+    in List.rev (loop words [])
 
 let neko_of_ops ops =
     List.map neko_of_op ops
@@ -86,15 +79,29 @@ let error msg =
     print_endline msg;
     exit 1
 
+(* Read source and break it into tokens *)
+let read_source filename =
+    let ops = ref [] in
+    let chan = open_in filename in
+    try
+        while true do
+            let line = input_line chan in
+            let line_tokens = Str.split (Str.regexp " ") line in
+            let line_ops = ops_of_tokens line_tokens in
+            ops := !ops @ line_ops
+        done; []
+    with End_of_file ->
+      close_in chan;
+    !ops
+
 let () =
     if (Array.length Sys.argv) <= 1 then error "Specify the file, nyan!"
     else
         begin
-            let source = read_source Sys.argv.(1) in
-            if check_nesting_level source > 0 then
+            let pseudo_ast = read_source Sys.argv.(1) in
+            if check_nesting_level pseudo_ast > 0 then
             error "Unmatched bracket, nyan!";
 
-            let pseudo_ast = ops_of_tokens source in
             let translated_source = String.concat "\n" (neko_of_ops pseudo_ast) in
             let output = prologue ^ translated_source in
             print_string output
